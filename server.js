f0ab291e14750c1ffbf535fc7fb150f73eb2d9e7
sonarct <sonarct@gmail.com>
2017-08-02 18:52:55 +0300

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const port = process.env.port || 80
+const port = process.env.port || 3000
 const bodyParser = require('body-parser')
 const fs = require('fs')
 
